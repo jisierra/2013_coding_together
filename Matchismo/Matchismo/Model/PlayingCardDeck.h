a45f2d697a48769a02100f3c0bbaa5eec3ab8bdc
jose ignacio sierra <jisierra@gmail.com>
2013-01-28 11:00:22 +0100

    PlayingCardDeck class

---
+++ Matchismo/Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by jisim on 1/28/13.
+//  Copyright (c) 2013 jisierra labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
