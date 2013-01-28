917ff3b484fc9903d8e7839b757197815198ab60
jose ignacio sierra <jisierra@gmail.com>
2013-01-28 10:08:13 +0100

    Card & Deck classes

--- Matchismo/Matchismo/Model/Card.h
+++ Matchismo/Matchismo/Model/Card.h
@@ -10,6 +10,10 @@
 
 @interface Card : NSObject
 
-@property (strong) NSString *contents;
+@property (nonatomic, strong) NSString *contents;
+@property (nonatomic, getter=isFaceUp) BOOL faceUp;
+@property (nonatomic, getter=isUnplayable) BOOL unplayable;
+
+- (int)match:(NSArray *)card;
 
 @end
